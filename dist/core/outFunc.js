bf2469b5d0359d24612d91ff741f5ecca45b348e
ekimovde <ekimov-denis@bauart.pro>
2021-04-17 13:51:45 +0700

    Fix

--- dist/core/outFunc.js
+++ dist/core/outFunc.js
@@ -7,14 +7,18 @@ exports.shiftOut = exports.replaceOut = void 0;
 
 var _Writer = require("../Writer");
 
+var _funcLengthText = require("./funcLengthText");
+
 var replaceOut = function replaceOut(container, i, writer) {
-  (0, _Writer.writeLine)(writer, "Text: ".concat(container[i].text, ", Replacement: ").concat(container[i].replacement, ", DecodedText: ").concat(container[i].decodedText));
+  var length = (0, _funcLengthText.funcLengthText)(container[i].text);
+  (0, _Writer.writeLine)(writer, "Text: ".concat(container[i].text, ", Length: ").concat(length, ", Replacement: ").concat(container[i].replacement, ", DecodedText: ").concat(container[i].decodedText));
 };
 
 exports.replaceOut = replaceOut;
 
 var shiftOut = function shiftOut(container, i, writer) {
-  (0, _Writer.writeLine)(writer, "Text: ".concat(container[i].text, ", Shift: ").concat(container[i].shift, ", DecodedText: ").concat(container[i].decodedText));
+  var length = (0, _funcLengthText.funcLengthText)(container[i].text);
+  (0, _Writer.writeLine)(writer, "Text: ".concat(container[i].text, ", Length: ").concat(length, ", Shift: ").concat(container[i].shift, ", DecodedText: ").concat(container[i].decodedText));
 };
 
 exports.shiftOut = shiftOut;
