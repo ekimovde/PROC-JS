38356482defed70664312976890d24a644b64ac4
ekimovde <ekimov-denis@bauart.pro>
2021-04-17 13:20:03 +0700

    Fix

--- src/core/outFunc.js
+++ src/core/outFunc.js
@@ -3,13 +3,20 @@ import { writeLine } from "../Writer";
 export const replaceOut = (container, i, writer) => {
   writeLine(
     writer,
-    `Text: ${container[i].text} Replacement: ${container[i].replacement}, DecodedText: ${container[i].decodedText}`
+    `Text: ${container[i].text}, Replacement: ${container[i].replacement}, DecodedText: ${container[i].decodedText}`
   );
 };
 
 export const shiftOut = (container, i, writer) => {
   writeLine(
     writer,
-    `Text: ${container[i].text} Shift: ${container[i].shift}, DecodedText: ${container[i].decodedText}`
+    `Text: ${container[i].text}, Shift: ${container[i].shift}, DecodedText: ${container[i].decodedText}`
+  );
+};
+
+export const replaceNumberOut = (container, i, writer) => {
+  writeLine(
+    writer,
+    `Text: ${container[i].text}, ReplaceNumber: ${container[i].replaceNumber}, DecodedText: ${container[i].decodedText}`
   );
 };
