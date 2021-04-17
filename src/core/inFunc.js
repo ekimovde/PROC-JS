5802283867c4ffc06ef28b702aea21c040626bb9
ekimovde <ekimov-denis@bauart.pro>
2021-04-17 13:20:46 +0700

    Fixed

--- src/core/inFunc.js
+++ src/core/inFunc.js
@@ -1,4 +1,12 @@
-import { replaceFunc, shiftFunc } from "../utils/encryption";
+import { replaceFunc, shiftFunc, replaceNumberFunc } from "../utils/encryption";
+
+export const replacementIn = (tmp, replacement, text) => {
+  replacement.text = text;
+  const { replaceTmp, replaceText } = replaceFunc(tmp);
+
+  replacement.replacement = replaceTmp.join("-");
+  replacement.decodedText = replaceText;
+};
 
 export const shiftIn = (tmp, shift, text) => {
   shift.text = text;
@@ -8,10 +16,10 @@ export const shiftIn = (tmp, shift, text) => {
   shift.decodedText = strEncoded;
 };
 
-export const replacementIn = (tmp, replacement, text) => {
-  replacement.text = text;
-  const { replaceTmp, replaceText } = replaceFunc(tmp);
+export const replaceNumberIn = (tmp, replaceNumber, text) => {
+  replaceNumber.text = text;
+  const { replaceTmp, replaceText } = replaceNumberFunc(tmp);
 
-  replacement.replacement = replaceTmp.join("-");
-  replacement.decodedText = replaceText;
+  replaceNumber.replaceNumber = replaceTmp.join("-");
+  replaceNumber.decodedText = replaceText;
 };
