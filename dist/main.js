998bcc92b044e559ae43c1bff378f88a2e49ce5a
ekimovde <ekimov-denis@bauart.pro>
2021-04-24 14:27:05 +0700

    Fix and added data validation

--- dist/main.js
+++ dist/main.js
@@ -13,16 +13,21 @@ function main() {
   (0, _Writer.writeLine)(writer, "Start!");
   var container = (0, _Container.containerConst)();
   var reader = new _Reader.Reader("in.txt", "", 0);
-  (0, _Reader.readerConst)("in.txt", reader);
-  (0, _Container.containerIn)(container, reader);
-  (0, _Container.containerOut)(container, writer);
-  (0, _Container.containerSort)(container);
-  (0, _Container.containerOut)(container, writer);
-  (0, _Container.containerOutReplacement)(container, writer);
-  (0, _Container.containerClear)(container);
-  (0, _Writer.writeLine)(writer, "Stop!");
-  console.log("Stop");
-  (0, _Writer.save)(writer);
+
+  try {
+    (0, _Reader.readerConst)("in.txt", reader);
+    (0, _Container.containerIn)(container, reader);
+    (0, _Container.containerOut)(container, writer);
+    (0, _Container.containerSort)(container);
+    (0, _Container.containerOut)(container, writer);
+    (0, _Container.containerOutReplacement)(container, writer);
+    (0, _Container.containerClear)(container);
+    (0, _Writer.writeLine)(writer, "Stop!");
+    console.log("Stop");
+    (0, _Writer.save)(writer);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 main();
